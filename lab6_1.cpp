d9ab4f095e999f624af633b4365564f702940400
lab314-32@labeng.local <lab314-32@labeng.local>
2020-12-25 15:34:28 +0700

    lab6_2 completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+using namespace std;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+int main() {
+    float a  =  20, b = 30;
+    float c = 4.0, output;
+
+    output = a - b/c;
+    cout << "output: "<< output << endl;
     return 0;
 }
